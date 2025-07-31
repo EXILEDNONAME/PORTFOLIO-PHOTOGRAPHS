de63d6b3e4b6b3878bb8f76468ac6ab7c63d5930
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-08-01 00:49:20 +0700

    2

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('pages.frontend.index'); });
+Route::get('/', function () { return view('pages.frontend.demo-1.index'); });
 
 require __DIR__.'/auth.php';
 require __DIR__.'/backend/dashboard.php';
